3547a1a9cef9f7108e765b6dd6ea215ab547f0b1
danjafish <danjafish@yandex.ru>
2021-08-26 19:08:34 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='effnetv2_model',
+      version='0.1',
+      description='Custom effnetv2 pytorch',
+      url='#',
+      author='danjafish',
+      license='MIT',
+      packages=['effnetv2_pytorch'],
+      zip_safe=False)
